943688137918afd9a39109278063f83d7aa4d95d
Scott-Neary <scott_neary@outlook.com>
2023-02-13 01:40:21 +0000

    OpenCV-Tutorial - Updated masking program

--- OpenCV-Tutorial/masking.py
+++ OpenCV-Tutorial/masking.py
@@ -8,10 +8,9 @@ blank = np.zeros(img.shape[:2], dtype='uint8')
 cv.imshow('Blank Image', blank)
 
 mask = cv.rectangle(blank, (img.shape[1]//2,img.shape[0]//2), (img.shape[1]//2 + 100,img.shape[0]//2 + 100), 255, -1)
-mask = cv.imshow('Mask', mask)
+# mask = cv.imshow('Mask', mask)
 
-# ! Problem - Image is not getting masked (1:56:00 of tutorial video)
-# ? Note - Image gets masked in histogram.py so something is not being run properly in this example
+# ? Note - Image gets masked in histogram.py so something may not run properly in this example
 masked = cv.bitwise_and(img,img,mask=mask)
 cv.imshow('Masked Image', masked)
 
