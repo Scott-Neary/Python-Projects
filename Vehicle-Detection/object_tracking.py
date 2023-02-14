7324324c55071642b2cb90354fb94dc017914ab8
Scott-Neary <scott_neary@outlook.com>
2023-02-14 15:26:48 +0000

    Object Tracking Tutorial - Add bounding box and circle for centre point

--- Vehicle-Detection/object_tracking.py
+++ Vehicle-Detection/object_tracking.py
@@ -9,20 +9,39 @@ od = ObjectDetection()
 # Load the video footage
 cap = cv.VideoCapture("Vehicle-Detection/Videos/los_angeles.mp4")
 
+# Initialise frame count
+count = 0
+center_points = []
+
 # Get the frames from the video in a loop
 while True:
-    _, frame = cap.read()
-
+    ret, frame = cap.read()
+    count += 1
+    if not ret:
+        break
+    
     # Detect objects on frame
     # class_ids = what type of object (car, truck, person etc)
     # scores = how confident the algorithm is about the detected object
     # boxes = the bounding box of each object
     (class_ids, scores, boxes) = od.detect(frame)
     for box in boxes:
-        print(box)
-
+        (x, y, w, h) = box
+        cx = int((x + x + w) / 2)
+        cy = int((y + y + h) / 2)
+        center_points.append((cx, cy))
+        print("FRAME No ", count, " ", x, y, w, h)
+        
+        # Draw full circle at the centre point of each object
+        # cv.circle(frame, (cx, cy), 5, (0,0,255), -1)
+        # Draw rectangle bounding box around objects
+        cv.rectangle(frame, (x, y), (x + w, y +h), (0, 255, 0), 2)
+    
+    for pt in center_points:
+        cv.circle(frame, pt, 5, (0,0,255), -1)
+    
     cv.imshow("Frame", frame)
-    key = cv.waitKey(1)
+    key = cv.waitKey(0)
     if key == 27:
         break
     
