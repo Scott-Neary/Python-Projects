3b36a33ed18b10ace879fa8b40b380d78f15c4dc
Scott-Neary <scott_neary@outlook.com>
2023-02-16 12:13:13 +0000

    Object Tracking Tutorial - Finalise vehicle tracking

--- Vehicle-Detection/object_tracking.py
+++ Vehicle-Detection/object_tracking.py
@@ -44,27 +44,54 @@ while True:
         # Draw rectangle bounding box around objects
         cv.rectangle(frame, (x, y), (x + w, y +h), (0, 255, 0), 2)
     
-    for pt in center_points_cur_frame:
-        for pt2 in center_points_prv_frame:
-            distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
+    # Only at the beginning we compare previous and current frame
+    if count <= 2:
+        for pt in center_points_cur_frame:
+            for pt2 in center_points_prv_frame:
+                distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
             
-            if distance < 10:
-                tracking_objects[track_id] = pt
-                track_id += 1
+                if distance < 20:
+                    tracking_objects[track_id] = pt
+                    track_id += 1
+    else:
+        
+        tracking_objects_copy = tracking_objects.copy()
+        center_points_cur_frame_copy = center_points_cur_frame.copy()
+        
+        for object_id, pt2 in tracking_objects_copy.items():
+            object_exists = False    
+            for pt in center_points_cur_frame_copy:
+                distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
+                
+                # Update IDs position
+                if distance < 20:
+                    tracking_objects[object_id] = pt
+                    object_exists = True
+                    if pt in center_points_cur_frame:
+                        center_points_cur_frame.remove(pt)
+                    continue
+                
+            # Remove IDs lost
+            if not object_exists:
+                tracking_objects.pop(object_id)
+    
+        # Add new IDs found
+        for pt in center_points_cur_frame:
+            tracking_objects[track_id] = pt
+            track_id += 1
     
     for object_id, pt in tracking_objects.items():
         cv.circle(frame, pt, 5, (0,0,255), -1)
-        # ! Warning - below line throws error at the moment
-        cv.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 0, 255), -2)          
+        cv.putText(frame, str(object_id), (pt[0], pt[1] - 7), 0, 1, (0, 0, 255), 1)          
     
-    print ("TRacking objects")
+    print ("Tracking objects")
     print(tracking_objects)
     
-    print("CUR FRAME")
+    print("CUR FRAME LEFT PTS")
     print(center_points_cur_frame)
     
-    print("PREV FRAME")
-    print(center_points_prv_frame)
+    # print("PREV FRAME")
+    # print(center_points_prv_frame)
     
     cv.imshow("Frame", frame)
     
