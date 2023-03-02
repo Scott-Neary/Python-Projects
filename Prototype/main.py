61e25801efa9dd9da52c231168fd5a69445cad00
Scott-Neary <scott_neary@outlook.com>
2023-03-02 16:17:37 +0000

    Updated prototype files

--- Prototype/main.py
+++ Prototype/main.py
@@ -1,14 +1,21 @@
 import cv2 as cv
 import numpy as np
+from object_tracking import VehicleTracking
 from detect_reg_class import RegDetectionClass
 
-# Read the image
-img = cv.imread('Car-Reg-Detection/Images/car5.jpg')
+# Read the video
+vid = cv.VideoCapture("Vehicle-Detection/Videos/cars3.mp4")
 
 # Instanstiate registration detection object
-reg_detect = RegDetectionClass(img)
+veh_tract = VehicleTracking(vid)
+veh_tract.track_vehicle()
 
-reg_detect.detect_reg_no()
+# Read the image
+# img = cv.imread('Car-Reg-Detection/Images/car5.jpg')
+
+# Instanstiate registration detection object
+# reg_detect = RegDetectionClass(img)
+# reg_detect.detect_reg_no()
 
 # Below code maybe for extracting objects
 # roi = frame[y: y +h, x: x + w]
