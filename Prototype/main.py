5971f3c94d645377990dbe8d2bc7232a83e6fe44
Scott-Neary <scott_neary@outlook.com>
2023-04-04 20:15:13 +0100

    Prototype - Updated main

--- Prototype/main.py
+++ Prototype/main.py
@@ -1,8 +1,5 @@
 import cv2 as cv
-import numpy as np
 from object_tracking import VehicleTracking
-from detect_reg_V2 import RegDetectionClassV2
-from db_class import Database
 
 # Read the video
 vid = cv.VideoCapture("Vehicle-Detection/Videos/cars3.mp4")
