83a442a79804faae901c771b299da4bbdbccb323
Scott-Neary <scott_neary@outlook.com>
2023-04-05 16:44:42 +0100

    Prototype - Remove unused code

--- Prototype/main.py
+++ Prototype/main.py
@@ -2,7 +2,7 @@ import cv2 as cv
 from object_tracking import VehicleTracking
 
 # Read the video
-vid = cv.VideoCapture("Prototype/Videos/cars2.mp4")
+vid = cv.VideoCapture("Prototype/Videos/cars3.mp4")
 
 # Instanstiate registration detection object
 veh_tract = VehicleTracking(vid)
